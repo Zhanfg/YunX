91658090ceac00e00d5fc404c1539dc3e854e14b
Axymorrsen <Axymosen@outlook.com>
2026-08-28 22:13:59 +0800

    test: cover public Google OneDrive and iCloud helpers

--- app/src/test/kotlin/com/yunx/app/data/provider/PublicShareResolverTest.kt
+++ app/src/test/kotlin/com/yunx/app/data/provider/PublicShareResolverTest.kt
@@ -21,36 +21,37 @@ class PublicShareResolverTest {
 
     @Test
     fun dropboxAddsDownloadParameterWhenMissing() = runBlocking {
-        val result = PublicShareResolvers.resolve(
-            "https://www.dropbox.com/s/abc/example.txt"
-        ).getOrThrow()
+        val result = PublicShareResolvers.resolve("https://www.dropbox.com/s/abc/example.txt").getOrThrow()
         assertTrue(result.downloadUrl.endsWith("?dl=1"))
     }
 
     @Test
     fun pCloudExtractsLongAndShortPublicCodesWithoutNetwork() {
-        assertEquals(
-            "ABC_def-123",
-            PublicShareResolvers.pCloudCode("https://e.pcloud.link/publink/show?code=ABC_def-123")
-        )
-        assertEquals(
-            "shortCode",
-            PublicShareResolvers.pCloudCode("https://pc.cd/shortCode")
-        )
+        assertEquals("ABC_def-123", PublicShareResolvers.pCloudCode("https://e.pcloud.link/publink/show?code=ABC_def-123"))
+        assertEquals("shortCode", PublicShareResolvers.pCloudCode("https://pc.cd/shortCode"))
         assertTrue(PublicShareResolvers.supports("pcloud"))
     }
 
     @Test
-    fun googleDriveExtractsCommonFileIdFormsWithoutClaimingDirectLink() {
-        assertEquals(
-            "abc_DEF-123",
-            PublicShareResolvers.googleDriveFileId("https://drive.google.com/file/d/abc_DEF-123/view?usp=sharing")
-        )
-        assertEquals(
-            "xyz_789",
-            PublicShareResolvers.googleDriveFileId("https://drive.google.com/open?id=xyz_789")
-        )
-        assertFalse(PublicShareResolvers.supports("google_drive"))
+    fun googleDriveExtractsFileAndWorkspaceIds() {
+        assertEquals("abc_DEF-123", PublicShareResolvers.googleDriveFileId("https://drive.google.com/file/d/abc_DEF-123/view?usp=sharing"))
+        assertEquals("xyz_789", PublicShareResolvers.googleDriveFileId("https://drive.google.com/open?id=xyz_789"))
+        assertEquals("doc_123", PublicShareResolvers.googleDriveFileId("https://docs.google.com/document/d/doc_123/edit"))
+        assertTrue(PublicShareResolvers.supports("google_drive"))
+    }
+
+    @Test
+    fun icloudExtractsOnlyDrivePublicId() {
+        assertEquals("0afGK6zDBog_0drwp6YZoDLIg", PublicShareResolvers.iCloudShortId("https://www.icloud.com/iclouddrive/0afGK6zDBog_0drwp6YZoDLIg#Patches"))
+        assertEquals(null, PublicShareResolvers.iCloudShortId("https://www.icloud.com/photos/abc"))
+        assertTrue(PublicShareResolvers.supports("icloud_drive"))
+    }
+
+    @Test
+    fun newPublicProvidersAreRegistered() {
+        assertTrue(PublicShareResolvers.supports("onedrive"))
+        assertTrue(PublicShareResolvers.supports("google_drive"))
+        assertTrue(PublicShareResolvers.supports("icloud_drive"))
     }
 
     @Test
